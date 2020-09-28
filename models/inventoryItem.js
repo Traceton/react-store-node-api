c9384ca29b17d4deeca97b08f2f421ef55cd0012
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-09-28 15:08:17 -0400

    changed itemId to a string in model

--- models/inventoryItem.js
+++ models/inventoryItem.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const inventoryItemSchema = new mongoose.Schema({
   itemId: {
-    type: Number,
+    type: String,
     required: false,
   },
   itemName: {
