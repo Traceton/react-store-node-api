8e1261a4f143ab1471ddd26e936151db41965132
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-10-13 23:29:18 -0400

    changed routes to be secure and only return certain data if passwords or req.parameters are correct.

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,11 @@
 const express = require("express");
-
 const router = express();
 const multer = require("multer");
 const upload = multer();
 const User = require("../models/user");
 
-// find user middleware function
-let findUserById = async (req, res, next) => {
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (!user) {
-      res.status(404).json({ message: "Could not find user" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-  res.user = user;
-  next();
-};
-
 let findUserByUsername = async (req, res, next) => {
   let user;
-
   try {
     user = await User.find({ username: req.params.username });
     if (!user) {
@@ -41,32 +24,63 @@ let findUserByUsername = async (req, res, next) => {
   next();
 };
 
-// get all users
-router.get("/", async (req, res) => {
+let verifyUserByUsernameAndPassword = async (req, res, next) => {
+  let user;
   try {
-    const users = await User.find();
-    res.status(201).json(users);
+    user = await User.find({ username: req.params.username });
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+      console.log(`user not found in findByUsername`);
+      return;
+    } else if (
+      req.params.username === user[0].username &&
+      req.params.password === user[0].password
+    ) {
+      res.user = user[0];
+    } else {
+      res.status(404).json({ message: "Incorrect Password" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
+    console.log(`error in findByUsername`);
   }
-});
-// get single user by id
-router.get("/:id", findUserById, (req, res) => {
+
+  console.log(`req.params.username-> ${req.params.username}`);
+  console.log(`findbyusername res.user -> ${res.user}`);
+
+  next();
+};
+
+// this call should only return public contact info.
+// get public info user by username
+router.get("/login/publicProfile/:username", findUserByUsername, (req, res) => {
+  const publicProfile = {
+    username: res.user.username,
+    firstName: res.user.firstName,
+    lastName: res.user.lastName,
+    email: res.user.email,
+    phone: res.user.phone,
+  };
+
   try {
-    res.status(201).json(res.user);
+    res.status(201).json(publicProfile);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// get user by username
-router.get("/login/:username", findUserByUsername, (req, res) => {
-  try {
-    res.status(201).json(res.user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+// login user with username and passsword
+router.get(
+  "/login/:username/:password",
+  verifyUserByUsernameAndPassword,
+  (req, res) => {
+    try {
+      res.status(201).json(res.user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
-});
+);
 
 // create a single user
 router.post("/", upload.single(), async (req, res) => {
@@ -87,35 +101,10 @@ router.post("/", upload.single(), async (req, res) => {
   }
 });
 // // delete a single user
-router.delete("/:id", findUserById, async (req, res) => {
+router.delete("/:username", findUserByUsername, async (req, res) => {
   try {
     await res.user.remove();
     res.status(201).json({ message: "user was deleted" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// update a single user by id
-router.patch("/:id", upload.single(), findUserById, async (req, res) => {
-  if (req.body.firstName != null) {
-    res.user.firstName = req.body.firstName;
-  }
-  if (req.body.lastName != null) {
-    res.user.lastName = req.body.lastName;
-  }
-  if (req.body.username != null) {
-    res.user.username = req.body.username;
-  }
-  if (req.body.password != null) {
-    res.user.password = req.body.password;
-  }
-  if (req.body.email != null) {
-    res.user.email = req.body.email;
-  }
-  try {
-    const updatedUser = await res.user.save();
-    res.status(201).json(updatedUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -159,5 +148,63 @@ router.patch(
     }
   }
 );
+
+// find user middleware function
+// let findUserById = async (req, res, next) => {
+//   let user;
+//   try {
+//     user = await User.findById(req.params.id);
+//     if (!user) {
+//       res.status(404).json({ message: "Could not find user" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+//   res.user = user;
+//   next();
+// };
+
+// // get all users
+// router.get("/", async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     res.status(201).json(users);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+// get single user by id
+// router.get("/:id", findUserById, (req, res) => {
+//   try {
+//     res.status(201).json(res.user);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+// update a single user by id
+// router.patch("/:id", upload.single(), findUserById, async (req, res) => {
+//   if (req.body.firstName != null) {
+//     res.user.firstName = req.body.firstName;
+//   }
+//   if (req.body.lastName != null) {
+//     res.user.lastName = req.body.lastName;
+//   }
+//   if (req.body.username != null) {
+//     res.user.username = req.body.username;
+//   }
+//   if (req.body.password != null) {
+//     res.user.password = req.body.password;
+//   }
+//   if (req.body.email != null) {
+//     res.user.email = req.body.email;
+//   }
+//   try {
+//     const updatedUser = await res.user.save();
+//     res.status(201).json(updatedUser);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 module.exports = router;
