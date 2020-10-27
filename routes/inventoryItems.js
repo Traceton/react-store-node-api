9e037a26f769b8ff43d0458f99031f13f26e0805
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-10-27 02:16:45 -0400

    fixed item id typo

--- routes/inventoryItems.js
+++ routes/inventoryItems.js
@@ -74,7 +74,7 @@ router.post(
   async (req, res) => {
     const user = await User.find({ userId: req.params.userId });
     const newInventoryItem = await new InventoryItem({
-      itemId: req.params.itemId,
+      itemId: req.body.itemId,
       itemName: req.body.itemName,
       itemCategory: req.body.itemCategory,
       itemDescription: req.body.itemDescription,
